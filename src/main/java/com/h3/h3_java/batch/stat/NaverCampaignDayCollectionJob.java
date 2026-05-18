f62a3accb4bea21ddf726713525f940afb523530
ChoiYoungWoo2197 <duddn2197@naver.com>
2026-05-18 15:32:23 +0900

    debug: 캠페인 일별 API 응답 구조 확인용 로그 추가

--- src/main/java/com/h3/h3_java/batch/stat/NaverCampaignDayCollectionJob.java
+++ src/main/java/com/h3/h3_java/batch/stat/NaverCampaignDayCollectionJob.java
@@ -87,6 +87,8 @@ public class NaverCampaignDayCollectionJob {
             }
 
             Map<String, Object> res = getStats(client, campaignId, date);
+            log.info("[NaverCampaignDay][DEBUG] campaignId={} date={} response={}", campaignId, date, res);
+
             if (res == null || !res.containsKey("data")) continue;
 
             @SuppressWarnings("unchecked")
@@ -94,6 +96,8 @@ public class NaverCampaignDayCollectionJob {
             if (data == null || data.isEmpty()) continue;
 
             Map<String, Object> d = data.get(0);
+            log.info("[NaverCampaignDay][DEBUG] data[0]={}", d);
+
             long impCnt   = toLong(d.get("impCnt"));
             long clkCnt   = toLong(d.get("clkCnt"));
             long salesAmt = toLong(d.get("salesAmt"));
